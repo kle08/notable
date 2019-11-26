ec82b94ee26e38295026d95951a69ec0fdfa1aad
iamasianman <kevinle1991@gmail.com>
2019-11-25 17:22:20 -0800

    completed

---
+++ exampleData.js
@@ -0,0 +1,123 @@
+module.exports = {
+  physiciansList: [
+    {
+      id: 1,
+      first: 'Julius',
+      last: 'Hibbert',
+      email: 'jul.hibbert@notablehealth.com'
+    },
+    {
+      id: 2,
+      first: 'Algernop',
+      last: 'Krieger',
+      email: 'alg.krieger@notablehealth.com'
+    },
+    {
+      id: 3,
+      first: 'Nick',
+      last: 'Riviera',
+      email: 'nic.riviera@notablehealth.com'
+    }
+  ],
+  physiciansApt: [
+    {
+      physiciansId: 1,
+      aptID: 1,
+      patient: {
+        firstName: 'Sterling',
+        lastName: 'Archer'
+      },
+      date: '11/11/19',
+      time: '08:00AM',
+      kind: 'New Patient'
+    },
+    {
+      physiciansId: 1,
+      aptID: 2,
+      patient: {
+        firstName: 'Cyril',
+        lastName: 'Figis'
+      },
+      date: '11/11/19',
+      time: '09:00AM',
+      kind: 'New Patient'
+    },
+    {
+      physiciansId: 1,
+      aptID: 3,
+      patient: {
+        firstName: 'Ray',
+        lastName: 'Gilette'
+      },
+      date: '11/11/19',
+      time: '10:00AM',
+      kind: 'New Patient'
+    },
+    {
+      physiciansId: 2,
+      aptID: 1,
+      patient: {
+        firstName: 'Sterling',
+        lastName: 'Archer'
+      },
+      date: '11/11/19',
+      time: '08:00AM',
+      kind: 'New Patient'
+    },
+    {
+      physiciansId: 2,
+      aptID: 2,
+      patient: {
+        firstName: 'Cyril',
+        lastName: 'Figis'
+      },
+      date: '11/11/19',
+      time: '09:00AM',
+      kind: 'New Patient'
+    },
+    {
+      physiciansId: 2,
+      aptID: 3,
+      patient: {
+        firstName: 'Ray',
+        lastName: 'Gilette'
+      },
+      date: '11/11/19',
+      time: '10:00AM',
+      kind: 'New Patient'
+    },
+    {
+      physiciansId: 3,
+      aptID: 1,
+      patient: {
+        firstName: 'Sterling',
+        lastName: 'Archer'
+      },
+      date: '11/11/19',
+      time: '08:00AM',
+      kind: 'New Patient'
+    },
+    {
+      physiciansId: 3,
+      aptID: 2,
+      patient: {
+        firstName: 'Cyril',
+        lastName: 'Figis'
+      },
+      date: '11/11/19',
+      time: '09:00AM',
+      kind: 'New Patient'
+    },
+    {
+      physiciansId: 3,
+      aptID: 3,
+      patient: {
+        firstName: 'Ray',
+        lastName: 'Gilette'
+      },
+      date: '11/11/19',
+      time: '10:00AM',
+      kind: 'New Patient'
+    }
+  ]
+}
--- server/index.js
+++ server/index.js
@@ -1,8 +1,64 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+let { physiciansList, physiciansApt } = require('../exampleData');
 
-app.get('/', function (req, res) {
-  res.send('Hello')
+app.use(bodyParser());
+app.use(express.json());
+
+app.get('/physicians', function (req, res) {
+  res.send(physiciansList);
+});
+
+app.get('/physicians/appointments', function (req, res) {
+  const appt = physiciansApt.filter(app => {
+    return app.physiciansId === req.body.physiciansId && app.date === req.body.date;
+  })
+  res.send(appt);
+});
+
+app.delete('/physicians/appointments/:aptID', function (req, res) {
+  const remainsApt = physiciansApt.filter(apt => {
+    return apt.aptID != req.params.aptID;
+  });
+  physiciansApt = remainsApt;
+  res.send(physiciansApt);
+});
+
+app.post('/physicians/appointments', function (req, res) {
+  const physiciansId = req.body.physiciansId;
+  const time = req.body.time;
+  const date = req.body.date;
+  let maxID = -Infinity;
+  for (apt of physiciansApt) {
+    maxID = Math.max(apt.aptID, maxID);
+  }
+  let availableID = maxID + 1;
+
+  let min = time.substr(3, 2);
+  min = parseInt(min);
+  if (min % 15 !== 0) {
+    res.send('Appointment must be in intervals of 15 mins');
+  }
+  const scheduleCheck = physiciansApt.filter(apt => {
+    return apt.physiciansId == physiciansId && apt.time == time && apt.date == date;
+  });
+  if (scheduleCheck.length >= 3) {
+    res.send('Doctors is full at this time');
+  } else {
+    physiciansApt.push({
+      physiciansId,
+      aptID: availableID,
+      patient: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+      },
+      date,
+      time,
+      kind: req.body.kind
+    })
+  }
+  res.send(physiciansApt);
 });
 
 app.listen(3000, () => {
